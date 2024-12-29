e7663c0c761bd47aeaba9b895797d3430142b3a9
Fghfj <151946587+Fghfj@users.noreply.github.com>
2024-12-29 14:15:06 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,33 +1,13 @@
 <template>
   <footer>
     <div class="copyright">
-      <n-text class="description" v-html="packageJson.qqtext" @click="jumpLink(packageJson.qqchannel)" />
-      <n-text
-        class="author"
-        :depth="3"
-        v-html="packageJson.author"
-        @click="jumpLink(packageJson.github)"
-      />
+      <n-text>Copyright @ All rights reserved</n-text>
     </div>
-    <n-text
-      v-if="icp"
-      :depth="3"
-      class="icp"
-      v-html="icp"
-      @click="jumpLink('https://beian.miit.gov.cn/')"
-    />
   </footer>
 </template>
 
 <script setup>
-import packageJson from "@/../package.json";
-
-const icp = ref(import.meta.env.VITE_ICP ? import.meta.env.VITE_ICP : null);
-
-// 链接跳转
-const jumpLink = (url) => {
-  window.open(url);
-};
+// 不需要任何脚本
 </script>
 
 <style lang="scss" scoped>
@@ -42,28 +22,6 @@ footer {
   justify-content: center;
   .copyright {
     margin-bottom: 4px;
-    .description {
-      cursor: pointer;
-      &::after {
-        content: "@ Copyright By";
-        margin: 0 6px;
-      }
-    }
-  }
-  .author {
-    cursor: pointer;
-    transition: all 0.3s;
-    &:hover {
-      color: var(--n-code-text-color);
-    }
-  }
-  .icp {
-    font-size: 13px;
-    cursor: pointer;
-    transition: all 0.3s;
-    &:hover {
-      color: var(--n-code-text-color);
-    }
   }
 }
 </style>
