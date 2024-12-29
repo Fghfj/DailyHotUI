d5e72fdfb550fbfc99fff28f7fd3c860699bc261
Fghfj <151946587+Fghfj@users.noreply.github.com>
2024-12-29 14:56:40 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,85 +3,83 @@ import { defineStore } from "pinia";
 export const mainStore = defineStore("main", {
   state: () => {
     return {
-      // 系统主题
-      siteTheme: "light",
-      // 新闻类别
+      siteTheme: "dark",
       newsArr: [
-        {
-          label: "B站热搜",
-          value: "bilibili",
-          order: 0,
-          show: true,
-        },
         {
           label: "微博",
           value: "weibo",
-          order: 1,
-          show: true,
-        },
-        {
-          label: "知乎",
-          value: "zhihu",
-          order: 2,
-          show: true,
-        },
-        {
-          label: "AcFun日榜",
-          value: "acfun",
-          order: 3,
-          show: true,
-        },
-        {
-          label: "百度",
-          value: "baidu",
-          order: 4,
-          show: true,
-        },
-        {
-          label: "少数派",
-          value: "sspai",
-          order: 5,
-          show: true,
-        },
-        {
-          label: "IT之家",
-          value: "ithome",
-          order: 6,
-          show: true,
-        },
-        {
-          label: "澎湃新闻",
-          value: "thepaper",
-          order: 7,
-          show: true,
-        },
-        {
-          label: "今日头条",
-          value: "toutiao",
-          order: 8,
-          show: true,
-        },
-        {
-          label: "百度贴吧",
-          value: "tieba",
-          order: 9,
-          show: true,
-        },
-        {
-          label: "稀土掘金",
-          value: "juejin",
-          order: 10,
+          order: 0,
           show: true,
         },
         {
           label: "腾讯新闻",
           value: "tencent",
-          order: 11,
+          order: 1,
+          show: true,
+        },
+        {
+          label: "澎湃新闻",
+          value: "thepaper",
+          order: 2,
           show: true,
         },
         {
           label: "抖音",
           value: "douyin",
+          order: 3,
+          show: true,
+        },
+        {
+          label: "B站热搜",
+          value: "bilibili",
+          order: 4,
+          show: true,
+        },
+        {
+          label: "知乎",
+          value: "zhihu",
+          order: 5,
+          show: true,
+        },
+        {
+          label: "百度",
+          value: "baidu",
+          order: 6,
+          show: true,
+        },
+        {
+          label: "AcFun日榜",
+          value: "acfun",
+          order: 7,
+          show: true,
+        },
+        {
+          label: "少数派",
+          value: "sspai",
+          order: 8,
+          show: true,
+        },
+        {
+          label: "IT之家",
+          value: "ithome",
+          order: 9,
+          show: true,
+        },
+        {
+          label: "今日头条",
+          value: "toutiao",
+          order: 10,
+          show: true,
+        },
+        {
+          label: "百度贴吧",
+          value: "tieba",
+          order: 11,
+          show: true,
+        },
+        {
+          label: "稀土掘金",
+          value: "juejin",
           order: 12,
           show: true,
         },
@@ -128,17 +126,13 @@ export const mainStore = defineStore("main", {
           show: true,
         },
       ],
-      // 链接跳转方式
       linkOpenType: "open",
-      // 页头固定
       headerFixed: true,
-      // 时间数据
       timeData: null,
     };
   },
   getters: {},
   actions: {
-    // 更改系统主题
     setSiteTheme(val) {
       $message.info(`已切换至${val === "dark" ? "深色模式" : "浅色模式"}`, {
         showIcon: false,
